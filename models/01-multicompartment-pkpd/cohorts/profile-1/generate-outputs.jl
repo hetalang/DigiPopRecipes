5bd1f105fb243447f7df3d790775012dca39f1ad
Evgeny Metelkin <evgeny.metelkin@gmail.com>
2026-02-13 13:57:13 +0200

    geenate metrics

--- models/01-multicompartment-pkpd/cohorts/profile-1/generate-outputs.jl
+++ models/01-multicompartment-pkpd/cohorts/profile-1/generate-outputs.jl
@@ -1,21 +1,65 @@
-using HetaSimulator
+using HetaSimulator, Plots
 using CSV, DataFrames
 
-p = load_platform("models/01-multicompartment-pkpd")
+## load model and scenarios and run mean
 
+p = load_platform("models/01-multicompartment-pkpd")
 pool_1 = read_scenarios("models/01-multicompartment-pkpd/scenarios/pool-1.csv")
 add_scenarios!(p, pool_1)
 
-#p |> sim |> plot # just for test
+#p |> sim |> plot
 
-parameter_set = read_mcvecs("models/01-multicompartment-pkpd/cohorts/profile-1/vp-1.csv")
+## load parameters and run simulations
+
+parameter_set = read_mcvecs("data/01-multicompartment-pkpd/profile-1/vp-parameters-1.csv")
 
 mc_res = mc(p, parameter_set)
-plot(mc_res)
-
-ens = EnsembleSummary(mc_res; quantiles=[0.05,0.95])
-plot(ens)
 
 mc_df = mc_res |> DataFrame
+CSV.write("data/01-multicompartment-pkpd/profile-1/vp-outputs-personal-1.csv", mc_df)
 
-#CSV.write("data/01-multicompartment-pkpd/profile-1/vp-outputs/1.csv", mc_df)
+fig = plot(mc_res)
+savefig(fig, "data/01-multicompartment-pkpd/profile-1/vp-outputs-personal-1.png")
+
+#ens = EnsembleSummary(mc_res; quantiles=[0.05,0.95])
+#plot(ens)
+
+# mean-sd format for DigiPopData.jl
+
+dfs = []
+for endpoint_name in [:drug_c, :pd_output_1]
+    df = combine(
+        groupby(mc_df, [:scenario, :t]),
+        :t => (x -> string(endpoint_name, "_", lpad(x[1] |> Int, 3, '0'))) => "endpoint",
+        endpoint_name => mean => "metric.mean",
+        endpoint_name => std  => "metric.sd",
+    )
+    push!(dfs, df)
+end
+digipop_df = vcat(dfs...)
+
+digipop_df[!, "metric.type"] .= "mean_sd"
+digipop_df[!, "id"] .= "m" .* lpad.(string.(1:nrow(digipop_df)), 3, '0')
+select!(digipop_df, Not(:t))
+
+CSV.write("data/01-multicompartment-pkpd/profile-1/vp-outputs-mean_sd-1.csv", digipop_df)
+
+## quantile format for DigiPopData.jl
+
+dfs = []
+levels = [0.25, 0.5, 0.75]
+for endpoint_name in [:drug_c, :pd_output_1]
+    df = combine(
+        groupby(mc_df, [:scenario, :t]),
+        :t => (x -> string(endpoint_name, "_", lpad(x[1] |> Int, 3, '0'))) => "endpoint",
+        endpoint_name => (x -> join(quantile(x, levels), ";")) => "metric.values",
+    )
+    push!(dfs, df)
+end
+digipop_df = vcat(dfs...)
+digipop_df[!, "metric.levels"] .= join(levels, ";")
+digipop_df[!, "metric.type"] .= "quantile"
+digipop_df[!, "id"] .= "m" .* lpad.(string.(1:nrow(digipop_df)), 3, '0')
+select!(digipop_df, Not(:t))
+
+CSV.write("data/01-multicompartment-pkpd/profile-1/vp-outputs-quantile-1.csv", digipop_df)
--- models/01-multicompartment-pkpd/cohorts/profile-1/generate-parameters.jl
+++ models/01-multicompartment-pkpd/cohorts/profile-1/generate-parameters.jl
@@ -1,15 +1,19 @@
 using CSV, DataFrames
+using JSON3, Distributions
 
-## generate VP parameters of 1024, all log-normal for 
-# Vc_0, kel, kdist_p, kdist_t, Emin_1, Emax_1, EC50_1
-N_vp = 1024
-parameters_df = DataFrame(
-    Vc_0 = rand(Normal(log(5.5), 1e-1), N_vp) .|> exp,
-    kel = rand(Normal(log(3e-1), 1e-1), N_vp) .|> exp,
-    kdist_p = rand(Normal(log(7e-1), 1e-1), N_vp) .|> exp,
-    kdist_t = rand(Normal(log(1e-2), 1e-1), N_vp) .|> exp,
-    Emin_1 = rand(Normal(log(5.), 1e-1), N_vp) .|> exp,
-    Emax_1 = rand(Normal(log(10.), 1e-1), N_vp) .|> exp,
-    EC50_1 = rand(Normal(log(0.8), 1e-1), N_vp) .|> exp
-)
+# load from file and generate
+json = JSON3.read("models/01-multicompartment-pkpd/cohorts/profile-1/variability.json")
+size = json["size"]
+
+parameters_df = DataFrame()
+for rule in json["parameters"]
+    if rule["distribution"] == "lognormal"
+        mean, std = rule["parameters"]
+        name = rule["name"]
+        parameters_df[!, name] = rand(Normal(log(mean), std), size) .|> exp
+    else
+        error("Unsupported distribution: $(rule["distribution"])")
+    end
+end
+
 CSV.write("data/01-multicompartment-pkpd/profile-1/vp-parameters-1.csv", parameters_df)

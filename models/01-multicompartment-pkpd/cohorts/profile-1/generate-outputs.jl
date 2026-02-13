817b17b258edeba6a61bb3fee14d2ee6009994e0
Evgeny Metelkin <evgeny.metelkin@gmail.com>
2026-02-13 15:58:16 +0200

    simplify metrics geneation

--- models/01-multicompartment-pkpd/cohorts/profile-1/generate-outputs.jl
+++ models/01-multicompartment-pkpd/cohorts/profile-1/generate-outputs.jl
@@ -24,63 +24,48 @@ savefig(fig, "data/01-multicompartment-pkpd/profile-1/vp-outputs-personal-1.png"
 #ens = EnsembleSummary(mc_res; quantiles=[0.05,0.95])
 #plot(ens)
 
+long_df = stack(mc_df, [:drug_c, :pd_output_1], variable_name=:output)
+
 # mean-sd format for DigiPopData.jl
 
-dfs = []
-for endpoint_name in [:drug_c, :pd_output_1]
-    df = combine(
-        groupby(mc_df, [:scenario, :t]),
-        :t => (x -> string(endpoint_name, "_", lpad(x[1] |> Int, 3, '0'))) => "endpoint",
-        endpoint_name => mean => "metric.mean",
-        endpoint_name => std  => "metric.sd",
-    )
-    push!(dfs, df)
-end
-digipop_df = vcat(dfs...)
+df = combine(
+    groupby(long_df, [:scenario, :output, :t]),
+    [:output, :t] => ((x,y) -> string(x[1], "_", lpad(y[1] |> Int, 3, '0'))) => "endpoint",
+    :value => mean => "metric.mean",
+    :value => std  => "metric.sd",
+)
 
-digipop_df[!, "metric.type"] .= "mean_sd"
-digipop_df[!, "id"] .= "m" .* lpad.(string.(1:nrow(digipop_df)), 3, '0')
-select!(digipop_df, Not(:t))
+df[!, "metric.type"] .= "mean_sd"
+df[!, "id"] .= "m" .* lpad.(string.(1:nrow(df)), 3, '0')
 
-CSV.write("data/01-multicompartment-pkpd/profile-1/vp-outputs-mean_sd-1.csv", digipop_df)
+CSV.write("data/01-multicompartment-pkpd/profile-1/vp-outputs-mean_sd-1.csv", df)
 
 ## quartile format for DigiPopData.jl
 
-dfs = []
 levels = [0.25, 0.5, 0.75]
-for endpoint_name in [:drug_c, :pd_output_1]
-    df = combine(
-        groupby(mc_df, [:scenario, :t]),
-        :t => (x -> string(endpoint_name, "_", lpad(x[1] |> Int, 3, '0'))) => "endpoint",
-        endpoint_name => (x -> join(quantile(x, levels), ";")) => "metric.values",
-    )
-    push!(dfs, df)
-end
-digipop_df = vcat(dfs...)
-digipop_df[!, "metric.levels"] .= join(levels, ";")
-digipop_df[!, "metric.type"] .= "quantile"
-digipop_df[!, "id"] .= "m" .* lpad.(string.(1:nrow(digipop_df)), 3, '0')
-select!(digipop_df, Not(:t))
+df = combine(
+    groupby(long_df, [:scenario, :output, :t]),
+    [:output, :t] => ((x,y) -> string(x[1], "_", lpad(y[1] |> Int, 3, '0'))) => "endpoint",
+    :value => (x -> join(quantile(x, levels), ";")) => "metric.values",
+)
 
-CSV.write("data/01-multicompartment-pkpd/profile-1/vp-outputs-quartile-1.csv", digipop_df)
+df[!, "metric.levels"] .= join(levels, ";")
+df[!, "metric.type"] .= "quantile"
+df[!, "id"] .= "m" .* lpad.(string.(1:nrow(df)), 3, '0')
+
+CSV.write("data/01-multicompartment-pkpd/profile-1/vp-outputs-quartile-1.csv", df)
 
 ## quantile 10% format for DigiPopData.jl
 
-dfs = []
 levels = [0.1, 0.20, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
-for endpoint_name in [:drug_c, :pd_output_1]
-    df = combine(
-        groupby(mc_df, [:scenario, :t]),
-        :t => (x -> string(endpoint_name, "_", lpad(x[1] |> Int, 3, '0'))) => "endpoint",
-        endpoint_name => (x -> join(quantile(x, levels), ";")) => "metric.values",
-    )
-    push!(dfs, df)
-end
-digipop_df = vcat(dfs...)
-digipop_df[!, "metric.levels"] .= join(levels, ";")
-digipop_df[!, "metric.type"] .= "quantile"
-digipop_df[!, "id"] .= "m" .* lpad.(string.(1:nrow(digipop_df)), 3, '0')
-select!(digipop_df, Not(:t))
+df = combine(
+    groupby(long_df, [:scenario, :output, :t]),
+    [:output, :t] => ((x,y) -> string(x[1], "_", lpad(y[1] |> Int, 3, '0'))) => "endpoint",
+    :value => (x -> join(quantile(x, levels), ";")) => "metric.values",
+)
 
-CSV.write("data/01-multicompartment-pkpd/profile-1/vp-outputs-quantile-1.csv", digipop_df)
+df[!, "metric.levels"] .= join(levels, ";")
+df[!, "metric.type"] .= "quantile"
+df[!, "id"] .= "m" .* lpad.(string.(1:nrow(df)), 3, '0')
 
+CSV.write("data/01-multicompartment-pkpd/profile-1/vp-outputs-quantile-1.csv", df)

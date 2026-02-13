fd8061faf961fa7b763b2e26c1ef109be30c9056
Evgeny Metelkin <evgeny.metelkin@gmail.com>
2026-02-13 15:04:24 +0200

    qurtile vs quantile syntetic data

--- models/01-multicompartment-pkpd/cohorts/profile-1/generate-outputs.jl
+++ models/01-multicompartment-pkpd/cohorts/profile-1/generate-outputs.jl
@@ -44,7 +44,7 @@ select!(digipop_df, Not(:t))
 
 CSV.write("data/01-multicompartment-pkpd/profile-1/vp-outputs-mean_sd-1.csv", digipop_df)
 
-## quantile format for DigiPopData.jl
+## quartile format for DigiPopData.jl
 
 dfs = []
 levels = [0.25, 0.5, 0.75]
@@ -62,4 +62,25 @@ digipop_df[!, "metric.type"] .= "quantile"
 digipop_df[!, "id"] .= "m" .* lpad.(string.(1:nrow(digipop_df)), 3, '0')
 select!(digipop_df, Not(:t))
 
+CSV.write("data/01-multicompartment-pkpd/profile-1/vp-outputs-quartile-1.csv", digipop_df)
+
+## quantile 10% format for DigiPopData.jl
+
+dfs = []
+levels = [0.1, 0.20, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+for endpoint_name in [:drug_c, :pd_output_1]
+    df = combine(
+        groupby(mc_df, [:scenario, :t]),
+        :t => (x -> string(endpoint_name, "_", lpad(x[1] |> Int, 3, '0'))) => "endpoint",
+        endpoint_name => (x -> join(quantile(x, levels), ";")) => "metric.values",
+    )
+    push!(dfs, df)
+end
+digipop_df = vcat(dfs...)
+digipop_df[!, "metric.levels"] .= join(levels, ";")
+digipop_df[!, "metric.type"] .= "quantile"
+digipop_df[!, "id"] .= "m" .* lpad.(string.(1:nrow(digipop_df)), 3, '0')
+select!(digipop_df, Not(:t))
+
 CSV.write("data/01-multicompartment-pkpd/profile-1/vp-outputs-quantile-1.csv", digipop_df)
+
